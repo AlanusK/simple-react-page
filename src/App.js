ca5adc0f2cdf1ab7d058cbda800bc5cc056f06d0
Alanus Kajara <alan@Alanuss-MacBook-Pro.local>
2023-05-20 00:25:20 +0300

    update app as per task requirements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+// import { useParams } from 'react-router-dom';
 
 function App() {
+  // id can be captured by using useParams() hook object destructuring
+  // const { id } = useParams();
+
+  // with 'http://localhoat/Proposals/reczYWjkWR3z9t5mY?api_key=keypBba27XUclxBwj' as url, the following id will be captured;
+
+  const id = 'reczYWjkWR3z9t5mY';
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://api.airtable.com/v0/appxOuGZvte0xuK2S/Proposals/${id}?api_key=keypBba27XUclxBwj`);
+      const json = await response.json();
+      setData(json);
+    };
+
+    fetchData()
+      // catch any error
+      .catch(console.error);
+  }, [id]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Client Name</th>
+            <th>Description</th>
+            <th>Proposal</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{data?.fields?.Client_Name}</td>
+            <td>{data?.fields?.Description}</td>
+            <td>{data?.fields?.Name}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
